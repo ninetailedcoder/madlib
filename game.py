a813a7436350afd8e03fd5fbcaa3a7bcd69aed6e
ninetailedcoder <torydemaio@gmail.com>
2022-08-16 14:09:47 -0500

    small madlibs file

---
+++ game.py
@@ -0,0 +1,53 @@
+Adjective1=input('adj:')
+
+Adjective2=input('adj:')
+
+Adjective3=input('adj:')
+
+Adjective4=input('adj:')
+
+Adjective5=input('adj:')
+
+Noun1=input('noun:')
+
+Noun2=input('noun:')
+
+Noun3=input('noun:')
+
+Noun4=input('noun:')
+
+Noun5=input('noun:')
+
+Noun6=input('noun:')
+
+Noun7=input('noun:')
+
+Noun8=input('noun:')
+
+Number=input('number:')
+
+Plural_Noun=input('plural noun:')
+
+Plural_Noun2=input('plural noun:')
+
+Verb_Ending_in_S=input('verb:')
+Verb_Ending_in_S2=input('verb2:')
+
+
+
+madlib = f"""Lewis Caroll\'s classic, Alice\'s Adventures in Wonderland, as well as" \
+its {Adjective1}, Through the Looking {Adjective2},
+have enchanted both the young and the old {Adjective3}.for the
+last {Adjective4}.years. Alice\'s {Adjective5}. adventures begin when
+she {Noun1}. down a {Noun2}. hole and lands
+in a strange and topsy-turvy {Noun3}. There she discovers she
+can become a tall {Noun4}. or a small {Noun5}. simply by
+nibbling on alternate sides of a magic {Noun6}. In her travels
+through Wonderland, Alice {Noun7}.such remarkable
+characters as the White {Noun8}, the {Number}. Hatter,
+the Cheshire {Plural_Noun}, and even the Queen of {Plural_Noun2}.
+Unfortunately, Alice's adventure come to a {Verb_Ending_in_S}.end
+when Alice awakens from her {Verb_Ending_in_S2}.
+"""
+
+print(madlib)
